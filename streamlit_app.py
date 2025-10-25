bef442ad80a1009196fd329dbefbbe0f68159ead
Sanjeevraj <83357990+Sanjeevraj01@users.noreply.github.com>
2025-10-25 16:03:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.write("Hellooo 1/n I'm Back")
+st.write("Hellooo 1 \n I'm Back")
 
 st.write("This is line 1.\nThis is line 2.")
 st.markdown("This is line A.\nThis is line B.")
